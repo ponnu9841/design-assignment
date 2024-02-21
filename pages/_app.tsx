3d12e956834a50ded626577048443d85cdf88476
ponnu9841 <mailtoponnu2011@gmail.com>
2024-02-22 02:13:14 +0530

    font optimized

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Montserrat } from "next/font/google";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-const font = Montserrat({
-	style: ["normal", "italic"],
-	subsets: ["latin"],
-});
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -17,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<Provider store={store}>
-		  <main className={`${font.className} dark min-h-screen`}>
+		  <main className={`dark min-h-screen`}>
         	<Component {...pageProps} />
 		  </main>
 		</Provider>
